55f6d1de5204b7e70a91f31e21f48a5f70d8a835
nishant-madara <andoriyanishant.com>
2021-05-27 02:25:31 +0530

    CF Files Added

---
+++ CodeChef/CF677-D2-A.cpp
@@ -0,0 +1,35 @@
+//Vanya and Fence
+// CF677-D2-A
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, h;
+
+    int w = 0;
+
+    cin >> n;
+    cin >> h;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] <= h)
+        {
+            w++;
+        }
+        else if (a[i] > h)
+        {
+            w = w + 2;
+        }
+    }
+
+    cout << w;
+
+    return 0;
+}
