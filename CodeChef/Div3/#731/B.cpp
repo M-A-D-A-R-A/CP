f322deae4ee876bd3168940c8157679f18d7707d
nishant-madara <andoriyanishant@gmail.com>
2021-07-10 22:27:58 +0530

    div3/#731

---
+++ CodeChef/Div3/#731/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    string s ="";
+    int ascii;
+    cin>>t;
+    while (t--)
+    {
+        for (int i = 1; i < s.length(); i++)
+        {
+            s=s+s[i];
+        }
+        
+    }
+    
+}
