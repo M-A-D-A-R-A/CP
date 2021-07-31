363ea5f33142026e0df15c079d93ff3752731793
nishant-madara <andoriyanishant@gmail.com>
2021-07-31 22:00:50 +0530

    day 18

---
+++ CodeChef/Div2/EDU_CF/A/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pii <pair<int,int>
+#define vi vector <int>
+#define vp vector <pair<int,int>>
+#define vpp vector <pair<pair<int,int>,int>>
+#define si set<int>
+#define ll long long
+#define ld long double
+#define pqf priority_queue <int,vector <int>> 
+#define pqb priority_queue <int,vector <int>,greater<int>> 
+#define ps(x,y) fixed << setprecision(y) << x
+#define MOD 1e9
+#define PI 3.1415926535
+
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void solve()
+{
+    long long int n;
+    cin>>n;
+    n=(n+1)/2;
+    n=max<long long>(n,3);
+    cout<<n*5<<endl;
+    
+    
+}
+
+int main()
+{
+    IOS
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout);
+    #else
+    #endif
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
