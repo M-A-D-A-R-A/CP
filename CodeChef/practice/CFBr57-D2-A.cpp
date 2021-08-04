1c07ab06476188d0b3a33494fd9489796994d1ed
nishant-madara <andoriyanishant@gmail.com>
2021-08-04 23:15:44 +0530

    day 22

---
+++ CodeChef/practice/CFBr57-D2-A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pii <pair<int,int>
+#define vi vector <int>
+#define vp vector <pair<int,int>>
+#define vpp vector <pair<pair<int,int>,int>>
+#define si set<int>
+#define ll long long
+#define ld long double
+#define pqf priority_queue <int,vector <int>> 
+#define pqb priority_queue <int,vector <int>,greater<int>> 
+#define ps(x,y) fixed << setprecision(y) << x
+#define MOD 1e9
+#define PI 3.1415926535
+
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void solve()
+{
+    string a,b;
+    cin>>a;
+    cin>>b;
+    for (int i = 0; i < a.size(); i++)
+    {
+       cout<< (a[i]^b[i]);
+    }
+    
+}
+
+int main()
+{
+    IOS
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout);
+    #else
+    #endif
+    solve();
+    return 0;
+}
