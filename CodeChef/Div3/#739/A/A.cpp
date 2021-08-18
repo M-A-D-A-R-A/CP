6f93cdd53069b263d2bb79c6c5f631cb3b81753d
nishant-madara <andoriyanishant@gmail.com>
2021-08-19 00:11:20 +0530

    day 3

---
+++ CodeChef/Div3/#739/A/A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pii <pair<int,int>
+#define vi vector <int>
+#define vp vector <pair<int,int>>
+#define vpp vector <pair<pair<int,int>,int>>
+#define si set<int>
+#define ll long long
+#define ld long double
+#define pqf priority_queue <int,vector <int>> 
+#define pqb priority_queue <int,vector <int>,greater<int>> 
+#define ps(x,y) fixed << setprecision(y) << x
+#define MOD 1e9
+#define PI 3.1415926535
+
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void solve()
+{
+    int ar[1001];
+    int x = 0;
+    for (int i = 1;; i++)
+    {
+        if (x > 1000)
+            break;
+        if ((i % 3 == 0) || (i % 10 == 3))
+            continue;
+        else
+        {
+            ar[x] = i;
+            x++;
+        }
+    }
+ 
+    int k;
+    cin >> k;
+    cout << ar[k - 1] << endl;
+}
+
+int main()
+{
+    IOS
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout);
+    #else
+    #endif
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
