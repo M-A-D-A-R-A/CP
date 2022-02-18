7b4654d7961d0d13824a81473f9a3a6958dafa95
nishant-madara <andoriyanishant@gmail.com>
2022-02-18 16:37:01 +0530

    nvm

--- CodeChef/juniorsheet/CF677-D2-A.cpp
+++ CodeChef/juniorsheet/CF677-D2-A.cpp
@@ -5,31 +5,31 @@
 using namespace std;
 int main()
 {
-    int n, h;
+    // int n, h;
 
-    int w = 0;
+    // int w = 0;
 
-    cin >> n;
-    cin >> h;
+    // cin >> n;
+    // cin >> h;
 
-    int a[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] <= h)
-        {
-            w++;
-        }
-        else if (a[i] > h)
-        {
-            w = w + 2;
-        }
-    }
+    // int a[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (a[i] <= h)
+    //     {
+    //         w++;
+    //     }
+    //     else if (a[i] > h)
+    //     {
+    //         w = w + 2;
+    //     }
+    // }
 
-    cout << w;
+    // cout << w;
 
-    return 0;
+    // return 0;
 }
