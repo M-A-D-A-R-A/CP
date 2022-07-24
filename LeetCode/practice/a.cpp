0b2568393fbe9932cf5735c488bb548985ae3687
M-A-D-A-R-A <andoriyanishant@gmail.com>
2022-07-24 21:32:16 +0530

    IIITV questions

---
+++ LeetCode/practice/a.cpp
@@ -0,0 +1,29 @@
+#include<iostream> 
+using namespace std; 
+
+class ClassA {  
+public: 
+   ClassA(int ii = 0) : i(ii) {} 
+   void show() { cout << "i = " << i << endl;} 
+private: 
+   int i; 
+}; 
+
+class ClassB { 
+public: 
+   ClassB(int xx) : x(xx) {} 
+   operator ClassA() const { return ClassA(x); } 
+private: 
+   int x; 
+}; 
+
+void g(ClassA a) 
+{  a.show(); } 
+
+int main() { 
+ ClassB b(10); 
+//  g(b); 
+ g(20); 
+ getchar(); 
+ return 0; 
+}
