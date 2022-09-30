1919f4470203b06ccf8a6449a50c07c2dd651bad
M-A-D-A-R-A <andoriyanishant@gmail.com>
2022-09-30 23:47:16 +0530

    15.cpp

---
+++ S_CP_Sheet/A/15.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define pii <pair<int,int>
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define vpp vector<pair<pair<int, int>, int>>
+#define si set<int>
+#define ll long long
+#define ld long double
+#define pqf priority_queue<int, vector<int>>
+#define pqb priority_queue<int, vector<int>, greater<int>>
+#define ps(x, y) fixed << setprecision(y) << x
+#define MOD 1e9
+#define PI 3.1415926535
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int arraySum(int a[], int n)
+{
+    int initial_sum = 0;
+    return accumulate(a, a + n, initial_sum);
+}
+
+void solve()
+{
+    int k, n, w;
+    std::cin >> k >> n >> w;
+    k = w++ * w * k / 2 - n;
+    std::cout << (k > 0 ? k : 0);
+}
+
+int main()
+{
+    IOS
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+#endif
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+    return 0;
+}
